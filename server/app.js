1333429fbc7cf17e8c56da7ca0489cbe511ae024
Sungjae.Kwak <sungjae8970@gmail.com>
2022-07-03 14:17:36 +0900

    [Feat] 프로젝트 관련 라우트 처리코드추가

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const mysql = require("./mysql"); // 이폴더의 index.js 자동추적
 
 app.use(
   express.json({
-    limit: "50mb", // 최대 50메가
+    limit: "50mb" // 최대 50메가
   })
 ); // 클라이언트 요청 body를 json으로 파싱 처리
 
@@ -21,31 +21,49 @@ let sess = {
   cookie: {
     httpOnly: true, // document.cookie 해도 쿠키 정보를 볼 수 없음
     secure: false, // https
-    maxAge: 1000 * 60 * 60, // 쿠키가 유지되는 시간
+    maxAge: 1000 * 60 * 60 // 쿠키가 유지되는 시간
   },
-  userId: "",
+  userId: ""
 };
 
 app.use(session(sess));
 
 const corsOptions = {
   origin: "http://localhost:8080", // 허용할 도메인 설정
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 200
 };
-
-const manageRoute = require("./routes/manage");
-const commonRoute = require("./routes/common");
 
 app.use(cors(corsOptions));
 
-app.use("/api/manage", manageRoute); // 이 두줄 set덕분에  routes/manage.js 에서는 1만 써도 데이터 당겨옴.
-app.use("/api/common", commonRoute); // 이 두줄 set덕분에  routes/common.js 에서는 1만 써도 데이터 당겨옴.
+const manageRoute = require("./routes/manage");
+const commonRoute = require("./routes/common");
+const projectRoute = require("./routes/project"); // 프로젝트 모집 관련입니다.
+const reviewRoute = require("./routes/review"); // 프로젝트 후기 관련입니다.
+
+app.use(cors(corsOptions));
+
+app.use("/manage", manageRoute); // 이 두줄 set덕분에  routes/manage.js 에서는 1만 써도 데이터 당겨옴.
+app.use("/common", commonRoute); // 이 두줄 set덕분에  routes/common.js 에서는 1만 써도 데이터 당겨옴.
+app.use("/project/recruit", projectRoute);
+app.use("/project/review", reviewRoute);
+
+/****************************/
+/* common       공통sql      */
+/****************************/
+app.get("/common/deptList", async (req, res) => {
+  const deptList = await mysql.query("common_deptList"); //  함수정의시 키워드 async에 대응한 await 필수
+  res.send(deptList);
+});
+app.get("/common/stackList", async (req, res) => {
+  const stackList = await mysql.query("common_stackList");
+  res.send(stackList);
+});
 
 /****************************/
 /* project     프로젝트 메뉴  */
 /****************************/
 // projectId 기준 분야별 지원자 / 승인된 자 숫자 가져오기
-app.get("/api/project/applicantsPerDept", async (req, res) => {
+app.get("/project/applicantsPerDept", async (req, res) => {
   console.log("req==>" + req);
   const applicantsPerDept = await mysql.query("applicantsPerDept");
   res.send(applicantsPerDept);
