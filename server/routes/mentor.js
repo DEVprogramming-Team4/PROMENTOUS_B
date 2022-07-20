379c09b1d25be7d3371ed351f0cac5981b92ffce
Sungjae.Kwak <sungjae8970@gmail.com>
2022-07-20 15:53:26 +0900

    feat:메인페이지 멘토리스트 api구현

--- server/routes/mentor.js
+++ server/routes/mentor.js
@@ -3,10 +3,26 @@ const router = express.Router();
 const mysql = require("../mysql");
 const _ = require("lodash");
 
-/****
- * FOR MENTORINFO   +   MENTORING
- *
- */
+async function getRateData(mentorList) {
+  for (let i = 0; i < mentorList.length; i++) {
+    const response = await mysql.query("mentorRate", mentorList[i].user_id);
+    if (response[0] === undefined) {
+      mentorList[i].rateCount = 0;
+      mentorList[i].rateAVG = 0;
+    } else {
+      mentorList[i].rateCount = response[0].cnt;
+      mentorList[i].rateAVG = response[0].rateAVG;
+    }
+  }
+}
+
+/***** FOR MENTORINFO   +   MENTORING *****/
+router.get("/", async (req, res) => {
+  const mentorList = await mysql.query("mentorListDefault"); // [{}, ... , {}]
+  // const mentorID = await mysql.query("mentorID");
+  await getRateData(mentorList);
+  res.send(mentorList);
+});
 /* 페이지 선택해 멘토링정보 4단위로 가져오기 */
 router.post("/getMentorInfo", async (req, res) => {
   let numberForEachPage = 4; //페이지 당 몇개씩 나오는가
